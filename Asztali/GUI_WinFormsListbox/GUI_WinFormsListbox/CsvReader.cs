64b5355e3e34d1d59b555538777e199fa4f0e4be
kalifaszi01 <petbal586@logiker.hu>
2026-02-12 15:04:20 +0100

    izé

--- Asztali/GUI_WinFormsListbox/GUI_WinFormsListbox/CsvReader.cs
+++ Asztali/GUI_WinFormsListbox/GUI_WinFormsListbox/CsvReader.cs
@@ -9,6 +9,23 @@ namespace GUI_WinFormsListbox
 {
     public static class CsvReader
     {
-     
+        public static List<Gyumolcs> BetoltGyumolcsok(string fajlnev)
+        { 
+        List<Gyumolcs> lista = new List<Gyumolcs>();
+            using (var sr = new StreamReader(fajlnev, Encoding.UTF8))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream) 
+                {
+                    string sor = sr.ReadLine();
+                    string[] adatok = sor.Replace("\"","").Split(';');
+
+                    lista.Add(new Gyumolcs(int.Parse(adatok[0]), adatok[1], adatok[2], adatok[3], adatok[4], adatok[5]));
+
+                }
+            }
+            return lista;
+        }
+        public static List<Erkezes> BetoltErkezesek(string fajlnev)
     }
 }
--- Asztali/GUI_WinFormsListbox/GUI_WinFormsListbox/Form1.cs
+++ Asztali/GUI_WinFormsListbox/GUI_WinFormsListbox/Form1.cs
@@ -16,5 +16,10 @@ namespace GUI_WinFormsListbox
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
